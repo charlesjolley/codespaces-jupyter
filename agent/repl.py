7e552828fbda4e5282778ec9f0dbab5c35f5ed92
Charles Jolley <charles@tinman.capital>
2023-04-08 00:24:12 +0000

    fix lint error

--- agent/repl.py
+++ agent/repl.py
@@ -3,12 +3,14 @@
 import langchain
 from .front_door import FrontDoorAgent
 
-BASIC_PROFILE = "Hi. My name is Charles. I live in Manhattan Beach. I love espresso, mexican, and fine dining. I'm into Italian if it is very authentic."
+BASIC_PROFILE = """
+    Hi. My name is Charles. I live in Manhattan Beach. I love espresso, mexican, and fine dining. I'm into Italian if it is very authentic.
+    """
 
 
 def main():
     '''Launches the agent repl'''
-    # langchain.verbose = True
+    langchain.verbose = True
     agent = FrontDoorAgent.create()
     agent.run(input=BASIC_PROFILE)
 
